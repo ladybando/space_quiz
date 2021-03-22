9ad32fe2782e3de63815599c4e1ef6288d6efcd9
Ayana Bando <ayanaflax@yahoo.com>
2021-03-22 13:41:07 -0400

    add directory file and update html call

---
+++ js/directory.js
@@ -0,0 +1,46 @@
+const correct = [];
+const incorrect = [];
+const answers = [];
+
+// 2. Store the number of questions answered correctly
+let correctAnswers = 0;
+//next challenge **enter a positive message for none missed
+for ( let i = 0; i < quesAndAnswer.length; i++ ) {
+  let question = quesAndAnswer[i][0];
+  let answer = quesAndAnswer[i][1];
+  let response = prompt(`${question}`);
+  answers.push(answer);
+  if (response.includes(answer)){
+    correctAnswers++;
+    correct.push(question);
+  } else{
+    incorrect.push(question);
+  }
+}
+
+  function createListItems( arr ) {
+  let items = '';
+  for ( let i = 0; i < arr.length; i++ ) {
+    items += `<li>${ arr[i] }</li>`;
+  }
+  return items;
+}
+let html = `<h1>You got ${correctAnswers} answer(s) correct. </h1>
+
+<h2>You got these correct:</h2>
+  <ol>
+    ${createListItems(correct)} <br>
+  </ol>
+<h2>You missed these:</h2>
+  <ol>
+    ${createListItems(incorrect)} <br>
+  </ol>
+
+  <h2>The correct answers, in order of appearance, are :</h2>
+  <ol>
+  ${createListItems(answers)}<br>
+  </ol>
+`;
+// 4. Display the number of correct answers to the user
+const main = document.querySelector('main');
+main.insertAdjacentHTML('beforeend', html);
